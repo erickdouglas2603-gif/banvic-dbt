b8403cd324cc30f51dc52d20db61cf36d6bde050
Erick Santos <erick.douglas2603@gmail.com>
2026-08-04 22:46:39 +0000

    Finalizado a dimensão de clientes

--- models/Staging/erp_banvic/stg_erp__localidades.sql
+++ models/Staging/erp_banvic/stg_erp__localidades.sql
@@ -1,1 +1,20 @@
-select * from {{ source('erp', 'localidades') }}
+with
+    fonte_localidades as (
+        
+        SELECT * FROM {{ source('erp', 'localidades') }}
+
+    )
+
+
+  ,renomeado as (
+
+    SELECT 
+        COD_LOCALIDADE as pk_localidade
+        ,CAST(CIDADE as string) as cidade
+        ,CAST(UF as string) as UF
+    FROM fonte_localidades
+
+)
+
+SELECT * FROM renomeado
+
